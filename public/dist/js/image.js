e8387859fee30777ef4cfbbec3a304c91c7726d3
Gurunath pai <pai.gurunath@gmail.com>
2017-07-21 00:20:00 +0530

    new app changes

---
+++ public/dist/js/image.js
@@ -0,0 +1,13 @@
+setInterval(function(){
+ 
+ console.log("Image Removed");
+ $("#orginialImg").prop("src","");
+ $("#outputImg").prop("src","");
+ setTimeout(function(){
+console.log("Image Reloaded");
+ $("#orginialImg").prop("src","http://192.168.43.46:3000/dist/img/pothole/pothole1.jpg?foo=" + new Date().getTime());
+ $("#outputImg").prop("src","http://192.168.43.46:3000/dist/img/pothole/output.jpg?foo=" + new Date().getTime());
+
+},1500);
+ 
+},3000);
--- public/dist/js/map.js
+++ public/dist/js/map.js
@@ -1,46 +1,60 @@
-var latlongs = [];
-var latitude = "";
-var longitude = "";
-navigator.geolocation.getCurrentPosition(function(position) {
-  latitude = position.coords.latitude;
-  longitude = position.coords.longitude;
-  console.log(latitude + " " + longitude);
+function getNonZeroRandomNumber(){
+    var random = Math.floor(Math.random()*199) - 99;
+    if(random==0) return getNonZeroRandomNumber();
+    return random;
+}	
+      var pubnub = new PubNub({
+         publishKey: 'pub-c-d228b405-18a3-481f-a38e-04d2dd6d7c50',
+         subscribeKey: 'sub-c-39610e6a-6bd9-11e7-8f73-02ee2ddab7fe'
+      });
+
+      var channel = 'pubnub-mapbox' + getNonZeroRandomNumber();
+
+      eon.map({
+        pubnub: pubnub,
+        id: 'map',
+        mbToken: 'pk.eyJ1IjoiaWFuamVubmluZ3MiLCJhIjoiZExwb0p5WSJ9.XLi48h-NOyJOCJuu1-h-Jg',
+        mbId: 'ianjennings.l896mh2e',
+        channels: [channel],
+        connect: connect,
+        options: {
+          zoomAnimation: false,
+        },
+      });
+     
+
+     jQuery.extend({
+	   getValues: function(url){
+	      var result = null;
+	      $.ajax({
+	         url: url,
+	         type: 'get',
+	         async: false,
+	         success: function(data){ result = data },
+	
+	      });
+	    return result;
+	   }
 });
 
-setInterval(function(){
-	latitude = latitude + 0.001;
-	longitude = longitude + 0.001;
-	console.log(latitude + " " + longitude);
-	TrackmapData(latitude,longitude);
-},10000); 
+      function connect() {	
+        setInterval(function(){
+          var z1 = $.parseJSON($.getValues('http://192.168.43.46:8080/getLatLong'));
+          var point = {
+             latlng: [z1[0], z1[1]]
+          };
+          var new_point = JSON.parse(JSON.stringify(point));
 
-function TrackmapData(lat,lng) {			
-	var mylatlongs = new google.maps.LatLng(lat,lng);
-	latlongs.push(mylatlongs);
-		
-	newlatlongsLength = latlongs.length;
-	oldlatlongsLength = latlongs.length;
+          new_point.latlng = [
+            new_point.latlng[0],
+            new_point.latlng[1]
+          ];
 
+          pubnub.publish({
+            channel: channel,
+            message: [new_point]
+          });
 
-	var mapProp = {
-		zoom: 20,
-		center: latlongs[latlongs.length - 1],
-		mapTypeId: google.maps.MapTypeId.ROADMAP
-	};
-	var map = new google.maps.Map(document.getElementById('google-map-div'), mapProp);
+        }, 1000);
 
-	var trackPath = new google.maps.Polyline({
-		path: latlongs,
-		icons: [{
-			icon: {
-				path: google.maps.SymbolPath.FORWARD_OPEN_ARROW
-			},
-			repeat: '175px'
-		}],
-		geodesic: true,
-		strokeColor: '#FF0000',
-		strokeOpacity: 1.0,
-		strokeWeight: 2
-	});
-	trackPath.setMap(map);
-}
+      };
